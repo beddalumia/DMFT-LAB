514f1078c5978c3a8aa43384bf9004c3ed667a82
Gabriele Bellomia <gbellomia@live.it>
2022-04-24 11:23:59 +0200

    Start development of +plotDMFT module
    Added a working infrastructure for:
    
    - nicely plotting generic spectral functions, from QcmPlab files
    
    - crafting GIFs of such spectral plots from entire phase-lines, as built
      by the runDMFT module
    
    To be probably refined, but it works quite well already.
    
    —————————————————————————————————————————————————————————————————————————
    
    All the know-how about GIF building comes from the happy experiments made
    within github.com/bellomia/MOTTlab and github.com/bellomia/genuary2021

---
+++ +plotDMFT/spectral_frame.m
@@ -0,0 +1,63 @@
+function fig = spectral_frame(filename,style,varargin)
+%% SPECTRAL_GIF: Builds a GIF for the line-evolution of the spectral functions
+%
+%   >> plotDMFT.spectral_frame(filename,style,varargin)
+%
+%  filename : filename of the complex spectral function to be plotted
+%  style    : plotting style to be passed to internal 1d-plotter ['line','area','scatter']
+%  varargin : additional options passed to internal 1d-plotter (e.g. 'FaceColor', etc...)
+%  ------------------------------------------------------------------------
+
+    % Load complex function of real/imag frequency z
+    F = load(filename);
+
+    % Assign domain, real and imag part according to QcmPlab convention
+    z  = F(:,1);
+    rF = F(:,3);
+    iF = F(:,2);
+
+    % Init figure
+    fig = figure("Name",filename);
+
+    % Plot both real and imag parts of F
+    if ~exist('style','var'); style = 'area'; end
+    cplot(z,rF,style,varargin{:}); hold on
+    cplot(z,iF,style,varargin{:});
+
+    % Title, Legend, Labels
+    title(filename,'Interpreter','none');
+    legend('Real part','Imaginary part');
+    if      any(strfind(filename,'realw'))
+                xlabel('$\omega$','Interpreter','latex');
+    elseif  any(strfind(filename,'iw'))
+                xlabel('$i\omega$','Interpreter','latex');
+    end
+
+    % Limits
+    xlim([z(1),z(end)]);
+    try % To deal with self-energies
+    ylim([min(min(rF),min(iF)),max(max(rF),max(iF))]);
+    catch
+    ylim([-1,1]);
+    end
+
+end
+
+%% Custom overloaded 1d-plotter
+function cplot(x,y,style,varargin)
+
+    if     strcmp(style,'area')
+
+                area(x,y,'FaceAlpha',0.5,varargin{:});
+
+    elseif strcmp(style,'scatter')
+
+                scatter(x,y,'filled',varargin{:});
+
+    else   % default: line-plot
+
+                plot(x,y,varargin{:});
+
+    end
+
+end
---
+++ +plotDMFT/spectral_gif.m
@@ -0,0 +1,53 @@
+function spectral_gif(filename,style,dt,ulist,varargin)
+%% SPECTRAL_GIF: Builds a GIF for the line-evolution of the spectral functions
+%
+%   >> plotDMFT.spectral_gif(filename,style,dt,ulist,varargin)
+%
+%  filename : filename of the complex spectral function to be plotted
+%  style    : plotting style to be passed to plotDMFT.spectral_frame()
+%  dt       : delay-time for the GIF frames, in seconds
+%  ulist    : an array of values for Hubbard interaction U (could be empty!)
+%  varargin : additional options passed to plotDMFT.spectral_frame()
+%  ------------------------------------------------------------------------
+    if ~exist('ulist','var') || isempty(ulist)
+        [ulist, ~] = postDMFT.get_list('U'); 
+    else
+        ulist = sort(ulist);
+    end
+
+    Nu = length(ulist);
+
+    fprintf('Start GIF building...\n\n');
+
+    for iU = 1:Nu
+
+        % Check for <U=%f> directory
+        U = ulist(iU);
+        UDIR = sprintf('U=%f',U);
+        if ~isfolder(UDIR)
+            errstr = 'U_list file appears to be inconsistent: ';
+            errstr = [errstr,UDIR];
+            errstr = [errstr,' folder has not been found.'];
+            error(errstr);
+        end
+
+        % Enter directory
+        cd(UDIR); 
+
+        % Pick and plot the requested filename
+        frame = plotDMFT.spectral_frame(filename,style,varargin{:});
+
+        % Exit directory
+        cd('..');
+
+        % Push frame to gif file
+        plotDMFT.push_frame([filename,'.gif'],iU,Nu,dt,frame); close(frame);
+
+    end
+    
+    fprintf('...GIFs have been built.\n\n');
+
+end 
+    
+    
+    
