2ac1486f64721fc3d580063a5cf33ae719f027d3
Gabriele Bellomia <gbellomia@live.it>
2022-11-02 11:24:44 +0100

    Add `postDMFT.get_pure_states`
    [states, probs] = get_pure_states(suffix,check)
    
       states: a cell of pure-state vectors [states{i} is a complex array]
       probs: an array of pure-state probabilities [probs(i) = 𝓟(states{i})]
       suffix: an optional charvec / string, handling inequivalent filenames
       check: an optional boolean to activate the RDM crosscheck [default:F]
    
    We may change the return type for 'states' (a plain matrix could be more
    efficient, it depends on what we'll choose as our data structure for the
    full pure_state_line / to optimize plotting of these states). We'll see.
    
    The final goal is to plot something like: probs(i)*|states{i}(:)|^2, i.e.
    the weight in which each Fock-space basis state enters the groundstate of
    the impurity / cluster. This should clarify which configurations dominate
    the physics of the system.

---
+++ +postDMFT/get_pure_states.m
@@ -0,0 +1,50 @@
+function [states, probs] = get_pure_states(suffix,check)
+%% Getting all information about pure states (cross-checking with the RDM)
+%
+%       [states, probs] = get_pure_states(suffix,check)
+%
+%  states: a cell of pure-state vectors [states{i} is a complex array]
+%  probs: an array of pure-state probabilities [probs(i) = 𝓟(states{i})]
+%  suffix: an optional charvec / string, handling inequivalent filenames
+%  check: an optional boolean to activate the RDM crosscheck [default:F]
+%  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    if(nargin<1)
+        suffix = [];
+    end
+    if(~isempty(suffix))
+        filename_p = ['probabilities_',suffix,'.dat'];
+        filename_s = ['pure-states_',suffix,'.dat'];
+    else
+        filename_p = 'probabilities.dat';
+        filename_s = 'pure-states.dat';
+    end
+    probs = load(filename_p);
+    psmat = load(filename_s);
+    if(nargin<2)
+        check = false; 
+    end
+    Npure = length(probs);
+    if(check)
+        filename_rdm = sprintf('reduced_density_matrix_rank%d.dat',Npure);
+        try
+            RDM = postDMFT.get_Hloc(filename_rdm);
+        catch
+            RDM = load(filename_rdm); % imag part may be omitted if zero
+        end
+        %% ACHTUNG: our convention [probs(i) = 𝓟(states{i})] implies that
+        %%          the matrix of states does not follow the convention of
+        %%          linear algebra, i.e. usually the eigenvector matrix is
+        %%          defined such that A=V*D*V', but for us A=V'*D*V [!🚨!]
+        RDM_check = psmat' * diag(probs) * psmat;
+        if any(abs(RDM-RDM_check)>1e-6)
+           error('Saved pure-states do not reconstruct the RDM!');
+        end
+    end
+    %% Reshape to cell
+    states = cell(Npure,1);
+    for i = 1:Npure
+       states{i} = psmat(i,:);
+    end
+end
+
+
