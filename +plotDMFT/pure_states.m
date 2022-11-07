5a5ad608a667a562dd9b5015936e35ad8a841d68
Gabriele Bellomia <gbellomia@live.it>
2022-11-07 06:07:04 +0100

    New version of `plotDMFT.pure_states` featuring a TREEMAP instead of PIE
    This allows to display in the same graph both the information about p(i)
    and |c{i}(j)|^2, i.e. what are the eigenvalues of the RDM defining the
    probabilities associated to pure-states and how the basis states enter
    these pure states. This is much more informative than the previous code
    based on a pie chart. For a broader discussion on the limitations of pie
    charts see also the commit message in 7b03f2aec3d12dde780c64a0ee20c40f25208a4c.

--- +plotDMFT/pure_states.m
+++ +plotDMFT/pure_states.m
@@ -6,10 +6,16 @@ function pure_states(suffix,check)
 %  suffix: a *required* charvec / string, handling inequivalent filenames
 %  check: an optional boolean to activate the RDM crosscheck [default:F]
 %
-%  ➜ it draws a pie chart with slices corresponding to p(i)*|c{i}(j)|^2,
+%  ➜ it draws a treemap with patches-area proportional to p(i)*|c{i}(j)|^2,
 %    where i spans the pure states, and p(i) is the associated probability
 %    and j spans the Fock-space basis, as defined in QcmPlab libraries.
-%    The c{i}(j) are basis expansion coefficients for the pure states.
+%    The c{i}(j) are basis expansion coefficients for the pure states, so
+%    we have each patch with an area that intuitively represents how much
+%    a configuration state (from the basis) contributes to the groundstate.
+%    Nevertheless we retain information about which are the pure states, by
+%    grouping such patches in bigger rectangles, each one corresponding to
+%    a single pure state, with area proportional to p(i). Suitable labels
+%    are assigned to the patches, when big enough to accomodate text.
 %  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     % Input checking
@@ -31,39 +37,29 @@ function pure_states(suffix,check)
     Nrdm = length(p);
     Norb = log2(Nrdm)/(2*Nlat); % Nrdm = 2^(2*Nlat*Norb)
 
-    % Build the basis state weights (and labels)
-    weights = zeros(Nrdm^2,1);
+    % Build the nested treemap: outer patches ~ p(i), inner ~ |c{i}(j)|^2
+    outer_tree = treemap(p);
+    plotDMFT.import_colorlab
+    colormap = get_palette('Spectral',Nrdm);
+    abs_weight = zeros(Nrdm,1);
     labels = cell(Nrdm^2,1);
     for i = 1:Nrdm
+        expansion = c{i}.*conj(c{i});
+        inner_tree = treemap(expansion,outer_tree(3,i),outer_tree(4,i));
+        inner_tree(1,:) = inner_tree(1,:) + outer_tree(1,i);
+        inner_tree(2,:) = inner_tree(2,:) + outer_tree(2,i);
+        % Assign labels, when appropriate (p(i)*|c{i}(j)|^2 > 1%)
         for j = 1:Nrdm
-            stride = j + (i-1) * Nrdm;
-            weights(stride) = p(i) * norm(c{i}(j))^2;
-            labels{stride} = build_ket(j-1,c{i}(j)); % BIN STARTS FROM 0
+            abs_weight(j) = p(i) * norm(c{i}(j))^2;
+            if abs_weight(j) > 0.01
+                labels{j} = build_ket(j-1,c{i}(j)); % BIN STARTS FROM 0
+            else
+                labels{j} = '';
+            end
         end
+        plotRectangles(inner_tree,labels,colormap);
     end
-    
-    % Setup pie-chart metadata
-    exploded = false(size(weights));
-    exploded(weights > 0.1) = true;
-    labels(weights < 0.1) = {''};
-
-    % Sort the slices
-    [weights,indices] = sort(weights);
-    exploded = exploded(indices);
-    labels = labels(indices);
-    
-    % Finally plot the pie
-    pie(weights,exploded,labels)
-
-    % And set decent colors
-    plotDMFT.import_colorlab
-    set_palette('lines')
-
-    % EXPERIMENTAL TREEMAP
-    figure
-    patches = treemap(weights);
-    plotRectangles(patches,labels,lines);
-    outline(patches);
+    outline(outer_tree,'-',2);
 
     %% FROM ED_SETUP:
     % |imp_up>|bath_up> * |imp_dw>|bath_dw>        <- 2*Nlat*Norb bits
@@ -107,8 +103,6 @@ function rectangles = treemap(data,w,h)
     %
     %   Copyright 2007-2013 The MathWorks, Inc.
     %
-    % default recursion limit of 500 is for wimps.
-    set(0,'RecursionLimit',5000)
     if (nargin == 0)
         % make up some random data
         data = abs(randn(1,80)) .^ 2;
@@ -269,12 +263,25 @@ function plotRectangles(rectangles,labels,colors)
     
     for i = 1:size(rectangles,2)
         r = rectangles(:,i);
+        if isnan(r(3)) || isnan(r(4)) || r(3)==0 || r(4)==0
+           continue 
+        end
         xPoints = [r(1), r(1), r(1) + r(3),r(1) + r(3)];
         yPoints = [r(2), r(2)+ r(4), r(2)+ r(4),r(2)];
+        if r(3) >= r(4)
+           angle = 0;
+        else
+           angle = 90;
+        end
         patch(xPoints,yPoints,colors(i,:),'EdgeColor','none');
         if(~isempty(labels))
             text(r(1) + r(3)/2,r(2) + r(4)/2, 1, labels{i}, ...
-                'VerticalAlignment','middle','HorizontalAlignment','center')
+                'VerticalAlignment','middle', ...
+                'HorizontalAlignment','center', ...
+                'Rotation',angle, ...
+                'FontSize',30*max(r(3),r(4)))
+                %'Color', [1 1 1], ...
+                %'BackgroundColor', colors(i,:)/3, ...
         end
     end
     
@@ -284,7 +291,7 @@ function plotRectangles(rectangles,labels,colors)
     
 end
 
-function outline(rectangles)
+function outline(rectangles,style,thickness)
     %OUTLINE  Outline in black all the rectangles
     %   outline(rectangles)
     %   rectangles is a 4-by-N matrix where each column is a rectangle in
@@ -299,11 +306,20 @@ function outline(rectangles)
     %
     %   See also PLOTRECTANGLES. 
     
+    if nargin < 2
+       style = '-';     % matlab's default
+    end
+    
+    if nargin < 3
+       thickness = 0.5; % matlab's default 
+    end
+    
     for i = 1:size(rectangles,2)
         r = rectangles(:,i);
         xPoints = [r(1),      r(1), r(1)+r(3), r(1)+r(3)];
         yPoints = [r(2), r(2)+r(4), r(2)+r(4), r(2)     ];
-        patch(xPoints,yPoints,[0 0 0],'FaceColor','none')
+        patch(xPoints,yPoints,[0 0 0],...
+            'FaceColor','none','LineWidth',thickness,'LineStyle',style)
     end
 end
     
