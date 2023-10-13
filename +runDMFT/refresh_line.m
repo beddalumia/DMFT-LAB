087329ae0dd66ce4ed4f77e97e1ce3cefb697e5a
Gabriele Bellomia <gbellomia@live.it>
2023-10-14 01:59:22 +0200

    Refactor `runDMFT.refresh_line` to take arbitrary main variable names
    Reasonably tested using "echo" as a pseudo-driver.

--- +runDMFT/refresh_line.m
+++ +runDMFT/refresh_line.m
@@ -1,15 +1,16 @@
-function refresh_line(EXE,doMPI,Nnew,where,varargin)
+function refresh_line(EXE,doMPI,Nnew,var,where,varargin)
 %% Refreshes a pre-existent calculation by Nnew loops [whole-line]
 %
-%   runDMFT.refresh_line(EXE,doMPI,Nnew,where,varargin)
+%   runDMFT.refresh_line(EXE,doMPI,Nnew,var,where,varargin)
 %
-%   EXE                 : Executable driver
-%   doMPI               : Flag to activate OpenMPI
-%   Nnew                : How much loops to add in the refresh
+%   EXE                 : Executable driver [mandatory]
+%   doMPI               : Flag to activate OpenMPI [default: true]
+%   Nnew                : How much loops to add in the refresh [default: 1]
+%   var                 : Main variable of the line [$(var)=%f directories]
 %   where               : Optional input defining whichˆ points to refresh
 %   varargin            : Set of fixed control parameters ['name',value]
 %
-% ˆ['all': all subfolders, 'conv': read U_conv.txt, 'nonconv': all - conv]
+% ˆ['all': all subfolders, 'conv': read $(var)_conv.txt, 'nonconv': all - conv]
 %  >> default string is 'nonconv'
 %  >> you shall pass even an array of floats instead of a string, in that
 %     case those would be the values to be refreshed (at your own risk).
@@ -20,11 +21,19 @@ if nargin < 1
    return
 end
 
-%% Open file to write/update converged U-values
-fileID_conv = fopen('U_conv.txt','a');
+%% Other optional input wrangling
+if nargin < 2
+   doMPI = true;
+end
+if nargin < 3
+   Nnew = 1;
+end
+if nargin < 4
+   var = 'U';
+end
 
 %% Default behavior if no provided where
-if nargin <= 3
+if nargin < 5
    mode = 'nonconv';
 elseif isnumeric(where)
    mode = 'custom';
@@ -32,38 +41,45 @@ else
    mode = where; 
 end
 
+%% Open file to write/update converged $(var)-values
+fconv_name = sprintf('%s_conv.txt',var);
+if(not(isfile(fconv_name)))
+   warning('You may have passed a wrong main variable name!')
+end
+fileID_conv = fopen(fconv_name,'a');
+
 %% Retrieve the list of *converged* U-values
-U_conv = unique(sort(load('U_conv.txt')));
+x_conv = unique(sort(load(fconv_name)));
 
 %% Retrieve the list of all available U-values
-U_list = postDMFT.get_list('U');
+x_list = postDMFT.get_list(var);
 
 %% Define which list of U-values refresh
 switch mode
     
     case 'all'
         
-        U = U_list;
+        X = x_list;
         
     case 'conv'
         
-        U = U_conv;
-        if isempty(U) 
+        X = x_conv;
+        if isempty(X) 
            warning('There are no converged points!')
+           return
         end
         
     case 'nonconv'
         
-        U = setdiff(U_list,U_conv);
-        if isempty(U) 
+        X = setdiff(x_list,x_conv);
+        if isempty(X) 
            warning('There are no unconverged points!')
+           return
         end
         
     case 'custom'
         
-        U = where;
-        fprintf('Refreshing the following points:')
-        disp(U)
+        X = where;
         
     otherwise
 
@@ -72,18 +88,22 @@ switch mode
         
 end
 
+%% Tell the user what we are refreshing...
+fprintf('Refreshing the following points:')
+disp(X)
+
 %% Phase-Line: single loop %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-for i = 1:length(U)
+for i = 1:length(X)
 
-    UDIR = sprintf('U=%f',U(i));   % Define the U-folder name.
+    DIR = sprintf('%s=%f',var,X(i));   % Define the $(var)-folder name.
 
-    if isfolder(UDIR)
-        cd(UDIR);                  % Enter the U-folder (if it exists)
+    if isfolder(DIR)
+        cd(DIR);                  % Enter the $(var)-folder (if it exists)
     else
-        errstr = 'U_list appears to be inconsistent: ';
-        errstr = [errstr,UDIR];
-        errstr = [errstr,' folder has not been found.'];
+        errstr = sprintf('%s_list appears to be inconsistent: ',var);
+        errstr = [errstr,DIR]; %#ok
+        errstr = [errstr,' folder has not been found.']; %#ok
         error(errstr);
     end
 
@@ -91,12 +111,12 @@ for i = 1:length(U)
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %% HERE WE CATCH A FAILED (unconverged) DMFT LOOP
-    if not(unconverged) && isempty(find(U_conv == U(i),1))
-        fprintf(fileID_conv,'%f\n', U(i));      % Update U_conv, only
+    if not(unconverged) && isempty(find(x_conv == X(i),1))
+        fprintf(fileID_conv,'%f\n', X(i));      % Update x_conv, only
     end                                         % if *newly* converged
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    cd ..                           % Exit the U-folder
+    cd ..                           % Exit the $(var)-folder
 
 end
 
